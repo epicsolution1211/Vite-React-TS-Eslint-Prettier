2357677fdd9a5fb05a12218078d6b9a7f824dacc
Your Name <your_email@example.com>
2023-10-08 16:29:23 +0300

    Initialize: vite eslint and prettier

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        width: "100vw",
+        height: "100vh",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <p style={{ fontSize: "24px" }}>
+        Vite React TypeScript Starter Project with Eslint and Prettier
+      </p>
+    </div>
+  );
+};
+
+export default App;
